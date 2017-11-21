db41989ffb4994366e60c900fae09d68cc749ead
univ3573 <alexander.morley@univ.ox.ac.uk>
2017-11-20 22:25:48 -0500

    initial commit

--- src/jPCA.jl
+++ src/jPCA.jl
@@ -1,5 +1,33 @@
 module jPCA
 
-# package code goes here
+export fit, JPCA
 
-end # module
+using MultivariateStats
+
+import MultivariateStats.fit
+
+type JPCA 
+    pca
+    M
+    Mskew
+    proj
+end
+
+"""
+    fit(jpca::Type{JPCA}, X, k=size(X,2))
+NB: X should be a time X features array.
+"""
+function fit(jpca::Type{JPCA}, X, k=size(X,2))
+    k > size(X,2) && error("k must be <= the number of columns in X")
+    
+    pca   = fit(PCA, X)
+    X2    = pca.proj[:,1:k]
+    
+    T     = (1:size(X2,1))[2:end]
+    Tpre  = (1:size(X2,1))[1:end-1]
+    M = (X2[T,:].-X2[Tpre,:])/X2[Tpre,:]
+
+    JPCA(pca, M, NaN, NaN)
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,9 @@ using jPCA
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+X = rand(100,100)
+Y = fit(JPCA, X, 10)
+
+@test size(Y.M,1) == size(Y.M,2)
+
+@test_throws ErrorException fit(JPCA, rand(5,5), 6)
